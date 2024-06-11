a9f4aeab978b4258232758209afbbf5d424739aa
RaphaelAZ <azevedo.raphael2017@gmail.com>
2024-06-11 10:58:33 +0200

    (+) articles list style

--- wp-content/themes/wordpress-cinema/index.php
+++ wp-content/themes/wordpress-cinema/index.php
@@ -3,9 +3,18 @@
 <main>
     <div class="container">
         <?php if ( have_posts() ) : ?>
+        <div class="articles-list">
         <?php while ( have_posts() ) : the_post(); ?>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <div class="article">
+                <div class="body">
+                    <div><?php the_content(); ?></div>
+                </div>
+                <div class="head">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </div>
+            </div>
         <?php endwhile; ?>
+        </div>
         <?php else : ?>
             <p>Aucun article trouvé</p>
         <?php endif; ?>
